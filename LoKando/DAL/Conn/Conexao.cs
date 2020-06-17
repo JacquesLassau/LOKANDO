5f9817b0ffbeec8306ffd96d2d9a5fab94effc27
JacquesLassau <37740292+JacquesLassau@users.noreply.github.com>
2020-06-17 08:40:36 -0300

    Configurada conexão para webconfig.

--- LoKando/DAL/Conn/Conexao.cs
+++ LoKando/DAL/Conn/Conexao.cs
@@ -1,15 +1,15 @@
 ﻿using System.Data.SqlClient;
 using System.Configuration;
+using System;
 
 namespace LoKando.DAL.Conn
 {
     public class Conexao
-    {
-        private const string stringConn = @"Server=DESKTOP-1T99VF9; Database=DB_LOKANDO; User Id=sa; Password=123@qwe;";
-
+    {        
+        private String connString = ConfigurationManager.ConnectionStrings["c22cea0060c8be6d840cf930d784e07b"].ConnectionString;
         public SqlConnection ConexaoDatabase()
         {
-            return new SqlConnection(stringConn);
+            return new SqlConnection(connString);
         }
     }
 }
